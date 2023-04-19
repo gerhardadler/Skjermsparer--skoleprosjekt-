b1d79626122f56f84007f9bffc73610ccb709f74
gerhardadler <gerhardskartveit@gmail.com>
2023-04-19 09:29:14 +0200

    Fixed so timeout is updated correctly

--- scripts/hide_nav_and_cursor.js
+++ scripts/hide_nav_and_cursor.js
@@ -1,25 +1,30 @@
 function resetHideTimeout(timeout, nav, body) {
     clearTimeout(timeout)
 
-        body.css("cursor", "auto")
-        nav.css("visibility", "visible")
+    body.css("cursor", "auto")
+    nav.css("visibility", "visible")
 
-        timeout = setTimeout(() => {
-            body.css("cursor", "none")
-            nav.css("visibility", "hidden")
-        }, 3000)
+    timeout = setTimeout(() => {
+        body.css("cursor", "none")
+        nav.css("visibility", "hidden")
+    }, 3000)
+    return timeout
 }
 
 $(document).ready(function () {
-
     const body = $("body");
     const nav = $(".include-nav-snippet");
     let timeout;
 
+    timeout = setTimeout(() => {
+        body.css("cursor", "none")
+        nav.css("visibility", "hidden")
+    }, 3000)
+
     document.addEventListener("mousemove", (event) => {
-        resetHideTimeout(timeout, nav, body)
+        timeout = resetHideTimeout(timeout, nav, body)
     })
     document.addEventListener("touchstart", (event) => {
-        resetHideTimeout(timeout, nav, body)
+        timeout = resetHideTimeout(timeout, nav, body)
     })
 })
